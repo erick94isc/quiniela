c21f694f0f5453ad3277f1f0c44349d15c005a17
Alan Mauricio <fernandez_cch@hotmail.com>
2019-05-24 09:39:20 -0500

    add equipo component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { EquiposComponent } from './equipos/equipos.component';
+import { NuevoEquipoComponent } from './nuevo-equipo/nuevo-equipo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EquiposComponent,
+    NuevoEquipoComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/nuevo-equipo/nuevo-equipo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nuevo-equipo',
+  templateUrl: './nuevo-equipo.component.html',
+  styleUrls: ['./nuevo-equipo.component.css']
+})
+export class NuevoEquipoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
