73527f9c1528d2562dd168ec191026aed43b7c2a
annicole <annicole_rosa@outlook.com>
2019-05-24 11:59:04 -0500

    add new componenents and routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,31 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { EquiposComponent } from './equipos/equipos.component';
 import { NuevoEquipoComponent } from './nuevo-equipo/nuevo-equipo.component';
+import {RouterModule,Routes} from '@angular/router';
+import { JugadoresComponent } from './jugadores/jugadores.component';
+import { NuevoJugadorComponent } from './nuevo-jugador/nuevo-jugador.component';
+import { HeaderComponent } from './header/header.component';
+
+
+const routes: Routes = [
+  {path:'',redirectTo:'/equipos',pathMatch:'full'},
+  {path:'equipos', component:EquiposComponent},
+  {path:'jugadores',component:JugadoresComponent},
+  {path:'nuevoEquipo',component:NuevoEquipoComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     EquiposComponent,
-    NuevoEquipoComponent
+    NuevoEquipoComponent,
+    JugadoresComponent,
+    NuevoJugadorComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
